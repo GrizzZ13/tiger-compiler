8898b0e16f243411bd33d3a3abecb29e86226f49
Grizz <1985661975@qq.com>
2021-11-20 18:43:11 +0800

    lab part1 escape analysis

--- src/tiger/escape/escape.cc
+++ src/tiger/escape/escape.cc
@@ -9,22 +9,31 @@ namespace absyn {
 
 void AbsynTree::Traverse(esc::EscEnvPtr env) {
   /* TODO: Put your lab5 code here */
+  root_->Traverse(env, 0);
 }
 
 void SimpleVar::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  esc::EscapeEntry* entry = env->Look(sym_);
+  if(entry && entry->depth_ < depth){
+    *(entry->escape_) = true;
+  }
 }
 
 void FieldVar::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  var_->Traverse(env, depth);
 }
 
 void SubscriptVar::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  var_->Traverse(env, depth);
+  subscript_->Traverse(env, depth);
 }
 
 void VarExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  var_->Traverse(env, depth);
 }
 
 void NilExp::Traverse(esc::EscEnvPtr env, int depth) {
@@ -41,34 +50,64 @@ void StringExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void CallExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  std::list<Exp*> expList = args_->GetList();
+  for(auto &exp : expList){
+    exp->Traverse(env, depth);
+  }
 }
 
 void OpExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  left_->Traverse(env, depth);
+  right_->Traverse(env, depth);
 }
 
 void RecordExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  std::list<EField*> efieldList = fields_->GetList();
+  for(auto &efield : efieldList){
+    efield->exp_->Traverse(env, depth);
+  }
 }
 
 void SeqExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  std::list<Exp*> expList = seq_->GetList();
+  for(auto &exp : expList){
+    exp->Traverse(env, depth);
+  }
 }
 
 void AssignExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  var_->Traverse(env, depth);
+  exp_->Traverse(env, depth);
 }
 
 void IfExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  test_->Traverse(env, depth);
+  then_->Traverse(env, depth);
+  if(elsee_){
+    elsee_->Traverse(env, depth);
+  }
 }
 
 void WhileExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  test_->Traverse(env, depth);
+  body_->Traverse(env, depth);
 }
 
 void ForExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  env->BeginScope();
+  escape_ = false;
+  env->Enter(var_, new esc::EscapeEntry(depth, &escape_));
+  lo_->Traverse(env, depth);
+  hi_->Traverse(env, depth);
+  body_->Traverse(env, depth);
+  env->EndScope();
 }
 
 void BreakExp::Traverse(esc::EscEnvPtr env, int depth) {
@@ -77,10 +116,19 @@ void BreakExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void LetExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  env->BeginScope();
+  std::list<Dec*> decList = decs_->GetList();
+  for(auto &dec : decList){
+    dec->Traverse(env, depth);
+  }
+  body_->Traverse(env, depth);
+  env->EndScope();
 }
 
 void ArrayExp::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  size_->Traverse(env, depth);
+  init_->Traverse(env, depth);
 }
 
 void VoidExp::Traverse(esc::EscEnvPtr env, int depth) {
@@ -89,10 +137,25 @@ void VoidExp::Traverse(esc::EscEnvPtr env, int depth) {
 
 void FunctionDec::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  depth++;
+  std::list<FunDec*> funDecList = functions_->GetList();
+  for(auto &funDec : funDecList){
+    env->BeginScope();
+    std::list<Field*> fieldList = funDec->params_->GetList();
+    for(auto &field : fieldList){
+      field->escape_ = false;
+      env->Enter(field->name_, new esc::EscapeEntry(depth, &(field->escape_)));
+    }
+    funDec->body_->Traverse(env, depth);
+    env->EndScope();
+  }
 }
 
 void VarDec::Traverse(esc::EscEnvPtr env, int depth) {
   /* TODO: Put your lab5 code here */
+  escape_ = false;
+  env->Enter(var_, new esc::EscapeEntry(depth, &escape_));
+  init_->Traverse(env, depth);
 }
 
 void TypeDec::Traverse(esc::EscEnvPtr env, int depth) {
--- src/tiger/frame/x64frame.h
+++ src/tiger/frame/x64frame.h
@@ -10,6 +10,53 @@
 namespace frame {
 class X64RegManager : public RegManager {
   /* TODO: Put your lab5 code here */
+public:
+  X64RegManager(){}
+
+  /**
+   * Get general-purpose registers except RSI
+   * NOTE: returned temp list should be in the order of calling convention
+   * @return general-purpose registers
+   */
+  [[nodiscard]] temp::TempList *Registers();
+
+  /**
+   * Get registers which can be used to hold arguments
+   * NOTE: returned temp list must be in the order of calling convention
+   * @return argument registers
+   */
+  [[nodiscard]] temp::TempList *ArgRegs();
+
+  /**
+   * Get caller-saved registers
+   * NOTE: returned registers must be in the order of calling convention
+   * @return caller-saved registers
+   */
+  [[nodiscard]] temp::TempList *CallerSaves();
+
+  /**
+   * Get callee-saved registers
+   * NOTE: returned registers must be in the order of calling convention
+   * @return callee-saved registers
+   */
+  [[nodiscard]] temp::TempList *CalleeSaves();
+
+  /**
+   * Get return-sink registers
+   * @return return-sink registers
+   */
+  [[nodiscard]] temp::TempList *ReturnSink();
+
+  /**
+   * Get word size
+   */
+  [[nodiscard]] int WordSize();
+
+  [[nodiscard]] temp::Temp *FramePointer();
+
+  [[nodiscard]] temp::Temp *StackPointer();
+
+  [[nodiscard]] temp::Temp *ReturnValue();
 };
 
 } // namespace frame
